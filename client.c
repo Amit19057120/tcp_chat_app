c422cf5f4004214c866822a94bbae1e8ee5000d8
Amit19057120 <amit08072005@gmail.com>
2023-01-03 19:03:20 +0530

    client code to be finished

---
+++ client.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+void error(const char *msg){
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[]){
+    int sockfd, portno,n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if(argc<3){
+        fprintf(stderr, "Usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno =atoi(argv[2]);
+    sockfd =socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd<0){
+        error("Error opening socket ");
+    }
+
+    }
+
